33b929e058f7a3f50aa6ea9f5e43d78881e414f9
andy <31009042+Davrosh@users.noreply.github.com>
2021-02-17 16:34:34 +0000

    add build 2

---
+++ docs/build/precache-manifest.4e19de732b6e78c952f2ec770b740630.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f039dc504d59a1f963b6b17653e46079",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ee7b8ea4faee8a3e0bae",
+    "url": "./static/css/main.ecfdfbc4.chunk.css"
+  },
+  {
+    "revision": "d36be6734218fcb24b00",
+    "url": "./static/js/2.80cdab33.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.80cdab33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee7b8ea4faee8a3e0bae",
+    "url": "./static/js/main.b71899c1.chunk.js"
+  },
+  {
+    "revision": "c0f66bce7e52d2b60ac8",
+    "url": "./static/js/runtime-main.f16cb788.js"
+  }
+]);
